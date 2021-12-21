be4b67321f971baa28ef00cc87421f1bdf2c47e3
NeverMind109 <vladgladkih123@gmail.com>
2021-12-21 16:39:47 +0300

    Created: Added Surf Section

--- app/js/main.js
+++ app/js/main.js
@@ -12,4 +12,19 @@ $(function(){
         dots: true,
         asNavFor: '.header__slider',
     });
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        nextArrow: '<img class="slider-arrows slider-item__info-next" src="images/arrow-right.svg" alt="Arrow: Right">',
+        prevArrow: '<img class="slider-arrows slider-item__info-prev" src="images/arrow-left.svg" alt="Arrow: Left">',
+        asNavFor: '.slider-map',
+    })
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.surf-slider',
+        focusOnSelect: true,
+    })
 });
