2ceff8cf191028242201b9fb81b9ef65681ca66c
NeverMind109 <vladgladkih123@gmail.com>
2021-12-23 03:07:07 +0300

    Created: Added Sleep Section

--- app/js/main.js
+++ app/js/main.js
@@ -27,10 +27,56 @@ $(function(){
         asNavFor: '.surf-slider',
         focusOnSelect: true,
     })
-    $('.travel__slider').slick({
+    $('.holder__slider').slick({
         infinite: true,
         fade: true,
         nextArrow: '<img class="slider-arrows slider-item__info-next" src="images/arrow-right.svg" alt="Arrow: Right">',
         prevArrow: '<img class="slider-arrows slider-item__info-prev" src="images/arrow-left.svg" alt="Arrow: Left">',
     })
+
+    $('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="images/plus.svg" alt="Star"></div><div class="quantity-button quantity-down"><img src="images/minus.svg" alt="Star"></div></div>').insertAfter('.quantity input');
+    $('.quantity').each(function() {
+      var spinner = $(this),
+        input = spinner.find('input[type="number"]'),
+        btnUp = spinner.find('.quantity-up'),
+        btnDown = spinner.find('.quantity-down'),
+        min = input.attr('min'),
+        max = input.attr('max');
+
+      btnUp.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue >= max) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue + 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+      btnDown.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue <= min) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue - 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+    });
+
+    $('.quantity-button').on('click', function(){
+      var parents = $(this).parents('.holder-slider__info');
+       let summ = $('.summ', parents).data('nights') * $('.nights', parents).val() + $('.summ', parents).data('guests') * $('.guests', parents).val();
+       $('.summ', parents).html('$' + summ + ' ');
+      });
+      
+// Для каждого слайда
+      $('.quantity').each(function() {
+      var parents = $(this).parents('.holder-slider__info');
+      let summ = $('.summ', parents).data('nights') * $('.nights', parents).val() + $('.summ', parents).data('guests') * $('.guests', parents).val();
+       $('.summ', parents).html('$' + summ + ' ');
+      });
 });
